1d16cdc23d7637308e79e5cffdadf7fccbca2f30
Somers Matthews <somersbmatthews@gmail.com>
2018-09-20 04:34:54 -0500

    hello world app made

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,9 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+def hello do
+  "hi there!"
 end
+
+end
+
+
